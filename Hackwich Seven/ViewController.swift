d63bbfb1f36d36e81693e9eb3041f952a81e3b02
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-10-18 00:43:47 -1000

    Completed Hackwich Seven
    for some reason made a tab bar at first lol

--- Hackwich Seven/ViewController.swift
+++ Hackwich Seven/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var typedtext : String {return textField.text ?? ""}
+    
+    @IBAction func setLabelButtonPressed(_ sender: Any)
+    {
+        self.displayLabel.text = typedtext
+        
+    }
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
+        self.displayLabel.text = ""
         
     }
 
